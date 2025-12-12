fbd66a9a0c8b09d92dca1d0e40cf4dbff24a86fa
NIBCarl <carlsteinborja3@gmail.com>
2025-12-12 08:53:16 +0800

    Delete CHECK_UPLOAD_LOGS.bat

--- CHECK_UPLOAD_LOGS.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "c:\Users\Engr. John Rome\Desktop\capstone\SNSU CAPSTONE\cafa-pos"
-powershell -Command "Get-Content storage\logs\laravel.log -Tail 20 | Select-String -Pattern 'Payment proof'"
-pause
